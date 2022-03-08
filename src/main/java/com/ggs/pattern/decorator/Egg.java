13a4967286d27b6be34f117e460645776aa0827c
lianghaohui <lianghaohui@ly-sky.com>
2022-03-08 13:56:13 +0800

    添加装饰者模式案例(这个很妙,多看深入学习)

---
+++ src/main/java/com/ggs/pattern/decorator/Egg.java
@@ -0,0 +1,24 @@
+package com.ggs.pattern.decorator;
+
+/**
+ * @Author lianghaohui
+ * @Date 2022/3/8 13:02
+ * @Description 鸡蛋类(具体的装饰者角色)
+ */
+public class Egg extends Garnish {
+
+    public Egg(FastFood fastFood) {
+        super(fastFood, 1, "鸡蛋");
+    }
+
+    @Override
+    public float cost() {
+        //计算价格
+        return super.getPrice() + getFastFood().cost();
+    }
+
+    @Override
+    public String getDesc() {
+        return super.getDesc() + getFastFood().getDesc();
+    }
+}
---
+++ src/main/java/com/ggs/pattern/decorator/FiredNoodles.java
@@ -0,0 +1,18 @@
+package com.ggs.pattern.decorator;
+
+/**
+ * @Author lianghaohui
+ * @Date 2022/3/8 12:58
+ * @Description 炒面(具体构件角色)
+ */
+public class FiredNoodles extends FastFood {
+
+    public FiredNoodles() {
+        super(12, "炒面");
+    }
+
+    @Override
+    public float cost() {
+        return getPrice();
+    }
+}
